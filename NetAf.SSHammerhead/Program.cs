377b16448263f766590bdcc91da4b15e66d21207
ben_singer <ben@singerinstruments.com>
2024-10-23 14:00:33 +0100

    Reformatted game

--- NetAf.SSHammerhead/Program.cs
+++ NetAf.SSHammerhead/Program.cs
@@ -1,42 +1,16 @@
 ﻿using System;
-using NetAF.Assets.Locations;
 using NetAF.Logic;
-using NetAF.SSHammerHead.Assets.Players;
+using NetAF.SSHammerhead;
 
 namespace NetAF.SSHammerHead
 {
     internal static class Program
     {
-        private static EndCheckResult CheckForGameOver(Game game)
-        {
-            if (!game.Player.IsAlive)
-                return new EndCheckResult(true, "You are dead.", "You are dead now.");
-
-            return EndCheckResult.NotEnded;
-        }
-
         private static void Main(string[] args)
         {
             try
             {
-                OverworldCreationCallback overworldCreator = () =>
-                {
-                    var overworldName = "CTY-1 Galaxy";
-                    var ship = new Assets.Regions.SSHammerHead.SSHammerHead().Instantiate();
-                    var overworld = new Overworld(overworldName, "A solar system in deep space, part of the SR389 galaxy.");
-                    overworld.AddRegion(ship);
-                    return overworld;
-                };
-
-                var game = Game.Create("Trouble aboard the SS HammerHead",
-                    "After years of absence, the SS Hammerhead reappeared in the delta quadrant of the CTY-1 solar system.\n\nA ship was hurriedly prepared and scrambled and made contact 27 days later.\n\nYou enter the outer most airlock and it closes behind you. With a sense of foreboding you see your ship detach from the airlock and retreat to a safe distance.",
-                    "This is a short demo game using NetAF.",
-                    overworldCreator,
-                    new Player().Instantiate,
-                    g => EndCheckResult.NotEnded,
-                    CheckForGameOver);
-
-                Game.Execute(game);
+                Game.Execute(TroubleAboardTheSSHammerHead.Create());
             }
             catch (Exception e)
             {
---
+++ NetAf.SSHammerhead/TroubleAboardTheSSHammerHead.cs
@@ -0,0 +1,55 @@
+﻿using NetAF.Assets.Locations;
+using NetAF.Logic;
+using NetAF.SSHammerHead.Assets.Players;
+
+namespace NetAF.SSHammerhead
+{
+    internal static class TroubleAboardTheSSHammerHead
+    {
+        #region Constants
+
+        private const string Title = "Trouble aboard the SS HammerHead";
+
+        private const string Introduction = "After years of absence, the SS Hammerhead reappeared in the delta quadrant of the CTY-1 solar system.\n\n"
+                                            + "A ship was hurriedly prepared and scrambled and made contact 27 days later.\n\n"
+                                            + "You enter the outer most airlock and it closes behind you. "
+                                            + "With a sense of foreboding you see your ship detach from the airlock and retreat to a safe distance.";
+
+        private const string Descritpion = "This is a short demo game using NetAF.";
+
+        #endregion
+
+        #region StaticMethods
+
+        private static EndCheckResult CheckForGameOver(Game game)
+        {
+            if (!game.Player.IsAlive)
+                return new EndCheckResult(true, "You are dead.", "You have succumb to the horrors of space.");
+
+            return EndCheckResult.NotEnded;
+        }
+
+        internal static GameCreationCallback Create()
+        {
+            OverworldCreationCallback overworldCreator = () =>
+            {
+                var overworldName = "CTY-1 Galaxy";
+                var ship = new SSHammerHead.Assets.Regions.SSHammerHead.SSHammerHead().Instantiate();
+                var overworld = new Overworld(overworldName, "A solar system in deep space, part of the SR389 galaxy.");
+                overworld.AddRegion(ship);
+                return overworld;
+            };
+
+            return Game.Create(Title,
+                               Introduction,
+                               Descritpion,
+                               overworldCreator,
+                               new Player().Instantiate,
+                               g => EndCheckResult.NotEnded,
+                               CheckForGameOver);
+
+        }
+
+        #endregion
+    }
+}
